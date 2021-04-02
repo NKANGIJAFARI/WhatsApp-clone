334e5db1cabbc5c705def4ce1a6337ab5ccd902b
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-02 10:42:21 +0400

    Added the sign In functionality  successfully

--- pages/login.js
+++ pages/login.js
@@ -2,13 +2,21 @@ import Head from 'next/head';
 import styled from 'styled-components';
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 import { Button } from '@material-ui/core';
+import { auth, provider } from '../firebase';
+
 const Login = () => {
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch(alert);
+  };
+
   return (
     <Container>
       <Head>Login</Head>
       <LoginContainer>
         <ChatLogo />
-        <Button variant='outlined'>Sign In with Google</Button>
+        <Button variant='outlined' onClick={signIn}>
+          Sign In with Google
+        </Button>
       </LoginContainer>
     </Container>
   );
@@ -26,12 +34,15 @@ const LoginContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 100px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0px 4px -3px rgba(0, 0, 0, 0.7);
 `;
 
 const ChatLogo = styled(WhatsAppIcon)`
   color: green;
   width: 100px;
-
+  margin-bottom: 30px;
   &&& {
     font-size: 100px;
   }
