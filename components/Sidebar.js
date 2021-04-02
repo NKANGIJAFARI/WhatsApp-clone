4d6c6b916358d0d610977d3c9a21509b9f5d8fad
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-02 08:53:33 +0400

    Styled the login page

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -27,9 +27,7 @@ const sidebar = () => {
           <IconButton>
             <ChatIcon />
           </IconButton>
-          <IconButton>
-            <MoreVertIcon />
-          </IconButton>
+          <IconButton>{/* <MoreVertIcon /> */}</IconButton>
         </IconContainer>
       </Header>
       <Search>
--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,40 @@
 import Head from 'next/head';
 import styled from 'styled-components';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import { Button } from '@material-ui/core';
 const Login = () => {
   return (
     <Container>
-      <Head>
-        <title>Login</title>
-      </Head>
+      <Head>Login</Head>
+      <LoginContainer>
+        <ChatLogo />
+        <Button variant='outlined'>Sign In with Google</Button>
+      </LoginContainer>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: whiteSmoke;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px;
+`;
+
+const ChatLogo = styled(WhatsAppIcon)`
+  color: green;
+  width: 100px;
+
+  &&& {
+    font-size: 100px;
+  }
+`;
 
 export default Login;
