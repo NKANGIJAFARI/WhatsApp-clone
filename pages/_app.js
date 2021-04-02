44c2a01f0625d2effdf900154f9bd12bfbc15ffa
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-02 11:28:22 +0400

    Added the dataase

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '../firebase';
+import firebase from 'firebase';
 import Login from './login';
 import Loading from '../components/Loading';
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users').doc(user.uid).set(
+        {
+          email: user.email,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true },
+      );
+    }
+  });
 
   if (loading) return <Loading />;
 
