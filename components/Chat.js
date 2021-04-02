dbf9c17954a9fe5028c82fe55bf9aa06bebe6678
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-02 13:15:02 +0400

    Chat component styled

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,29 @@
 import styled from 'styled-components';
+import { Avatar } from '@material-ui/core';
 
 const Chat = ({ id, users }) => {
   return (
-    <div>
-      <p>Recipient email</p>
-    </div>
+    <Container>
+      <UserAvatar />
+      <p>Name of user</p>
+    </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 5px;
+  word-break: break-word;
+
+  :hover {
+    background-color: #e9eaeb;
+  }
+`;
+
+const UserAvatar = styled(Avatar)`
+  margin: 0px 20px 0px 20px;
+`;
 
 export default Chat;
