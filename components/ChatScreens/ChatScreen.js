33072740a3c57a9ade2723e4660d758a61899fbc
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-03 07:55:39 +0400

    Added the header secytion and its icons, styling is next

--- components/ChatScreens/ChatScreen.js
+++ components/ChatScreens/ChatScreen.js
@@ -1,7 +1,62 @@
+import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import styled from 'styled-components';
-const ChatScreen = () => {
-  return <Container></Container>;
+import { auth } from '../../firebase';
+import { Avatar, IconButton } from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { AttachFile } from '@material-ui/icons';
+
+const ChatScreen = ({ chat, messages }) => {
+  const router = useRouter();
+  const [user] = useAuthState(auth);
+
+  return (
+    <Container>
+      <Header>
+        <Avatar></Avatar>
+
+        <HeaderInformation>
+          <h3>Recip email</h3>
+          <p>Last seen</p>
+        </HeaderInformation>
+
+        <HeaderIcons>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+    </Container>
+  );
 };
 export default ChatScreen;
 
 const Container = styled.div``;
+
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+  height: 80px;
+`;
+
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: grey;
+  }
+`;
