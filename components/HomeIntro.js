d6c59565601c49767bed143b7659e58b34725714
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-06 08:02:17 +0400

    Added an image and styling to the home into side

--- components/HomeIntro.js
+++ components/HomeIntro.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
+import Image from 'next/image';
 
 const HomeIntro = () => {
   return (
     <Container>
       <ImageWrapper>
-        <img src='images/chat.png' alt='' srcset='' />
+        <Image
+          src='/chat.png'
+          alt='Picture of the author'
+          width={400}
+          height={300}
+        />
       </ImageWrapper>
+      <DetailsContainer>
+        <h2>Stay Connected, Feel together</h2>
+        <p>
+          Through a fast and secure chat system, we assist you keep up with your
+          loved ones
+        </p>
+      </DetailsContainer>
     </Container>
   );
 };
@@ -19,4 +32,12 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const ImageWrapper = styled.div``;
+const ImageWrapper = styled.div`
+  width: 80%;
+  height: 60vh;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
