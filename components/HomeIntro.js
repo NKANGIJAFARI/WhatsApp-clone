83f697fa6421210f63c41c3987db1666b54100c5
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-06 07:31:28 +0400

    Added Home intro component for the first page.

---
+++ components/HomeIntro.js
@@ -0,0 +1,15 @@
+const HomeIntro = () => {
+  return (
+    <Container>
+      <ImageWrapper>
+        <img src='images/chat.png' alt='' srcset='' />
+      </ImageWrapper>
+    </Container>
+  );
+};
+
+export default HomeIntro;
+
+const Container = styled.div``;
+
+const ImagesWrapper = styled.div``;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styled from 'styled-components';
 
 import Sidebar from '../components/Sidebar';
 
@@ -10,7 +11,15 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Sidebar />
+      <Container>
+        <Sidebar />
+        <div>This is a chat</div>
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
