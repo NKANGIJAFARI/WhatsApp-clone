d6eb80780d5eea5ddb845a12c5fbe3586591198a
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-06 12:08:43 +0400

    Next, uploading to vercel

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <Main>
         <Head>
           <title>WhatsApp clone</title>
-          <link rel='icon' href='/favicon.ico' />
+          <link rel='icon' href='/chat.svg' />
         </Head>
 
         <Wrapper>
