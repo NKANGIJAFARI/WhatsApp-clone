4220ad520a1d725f033583b9ac075da2738079cb
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-04-07 08:16:29 +0400

    Recipient details given styling

--- components/ChatScreens/ChatScreen.js
+++ components/ChatScreens/ChatScreen.js
@@ -76,42 +76,6 @@ const Container = styled.div`
   flex-direction: row;
 `;
 
-const RecipientDetailsWrapper = styled.div`
-  min-width: 300px;
-  display: none;
-  transition: display 0.9s;
-  height: 100vh;
-  overflow: hidden;
-
-  &.active {
-    display: block;
-    height: 100vh;
-    overflow-y: hidden;
-    position: sticky;
-    top: 0;
-  }
-`;
-
-const RecipientDetails = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  padding: 60px 30px;
-  background-color: whiteSmoke;
-
-  & .details__avatar {
-    width: 150px;
-    height: 150px;
-  }
-`;
-
 const CancelIcon = styled(Cancel)`
   &&& {
     font-size: 2.5rem;
--- components/ChatScreens/RecipientDetails.js
+++ components/ChatScreens/RecipientDetails.js
@@ -20,3 +20,39 @@ const RecipientDetails = () => {
 };
 
 export default RecipientDetails;
+
+const RecipientDetailsWrapper = styled.div`
+  min-width: 300px;
+  display: none;
+  transition: display 0.9s;
+  height: 100vh;
+  overflow: hidden;
+
+  &.active {
+    display: block;
+    height: 100vh;
+    overflow-y: hidden;
+    position: sticky;
+    top: 0;
+  }
+`;
+
+const RecipientDetails = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 60px 30px;
+  background-color: whiteSmoke;
+
+  & .details__avatar {
+    width: 150px;
+    height: 150px;
+  }
+`;
